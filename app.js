4dfd95ab95d5cd223f52dd6ec0c828215501bf03
Do Cong Thanh <thanhdcgch190465@fpt.edu.vn>
2021-08-21 22:23:43 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,66 @@
+const express = require('express')
+
+
+const app = express()
+
+const { insertStudent, updateStudent, getStudentById, deleteStudent
+    , getDB, } = require('./databaseHandler');
+
+app.set('view engine', 'hbs')
+
+
+app.use(express.static('public'))
+
+app.get('/edit', async (req, res) => {
+    const id = req.query.id;
+
+    const s = await getStudentById(id);
+    res.render("edit", { student: s });
+})
+app.post('/update', async (req, res) => {
+    const nameInput = req.body.txtName;
+    const tuoiInput = req.body.txtTuoi;
+    const id = req.body.txtId;
+
+    updateStudent(id, nameInput, tuoiInput);
+    res.redirect("/");
+})
+
+app.post('/insert', async (req, res) => {
+    const nameInput = req.body.txtName;
+    const tuoiInput = req.body.txtTuoi;
+    const pictureInput = req.body.txtPicture;
+    if(nameInput.length <4){
+        res.render("index",{errorMsg:'Ten nho hon 4 ky tu'})
+        return;
+    }
+
+    const newStudent = { name: nameInput, tuoi: tuoiInput, picture: pictureInput }
+
+    insertStudent(newStudent);
+    res.redirect("/");
+})
+app.get('/delete', async (req, res) => {
+    const id = req.query.id;
+
+    await deleteStudent(id);
+    res.redirect("/");
+})
+
+app.get('/', async (req, res) => {
+    const dbo = await getDB();
+    const allStudents = await dbo.collection("students").find({}).toArray();
+    res.render('index', { data: allStudents })
+})
+
+app.post('/search', async (req, res) => {
+    const searchInput = req.body.txtSearch;
+    const dbo = await getDB()
+    const allStudents = await dbo.collection("students").find({ name: searchInput }).toArray();
+
+    res.render('index', { data: allStudents })
+})
+
+const PORT = process.env.PORT || 5001;
+app.listen(PORT)
+console.log("app is running ", PORT)
---
+++ databaseHandler.js
@@ -0,0 +1,38 @@
+const {ObjectId,MongoClient} = require('mongodb');
+const url = 'mongodb://localhost:27017';
+
+async function getDB() {
+    const client = await MongoClient.connect(url);
+    const dbo = client.db("GCH0805DB");
+    return dbo;
+}
+async function insertUser(newUser) {
+    const dbo = await getDB();
+    await dbo.collection("users").insertOne(newUser);
+}
+
+async function insertStudent(newStudent) {
+    const dbo = await getDB();
+    await dbo.collection("students").insertOne(newStudent);
+}
+
+async function updateStudent(id, nameInput, tuoiInput) {
+    const filter = { _id: ObjectId(id) };
+    const newValue = { $set: { name: nameInput, tuoi: tuoiInput } };
+
+    const dbo = await getDB ();
+    await dbo.collection("students").updateOne(filter, newValue);
+}
+async function getStudentById(id) {
+    const dbo = await getDB();
+    const s = await dbo.collection("students").findOne({ _id: ObjectId(id) });
+    return s;
+}
+async function deleteStudent(id) {
+    const dbo = await getDB();
+    await dbo.collection("students").deleteOne({ "_id": ObjectId(id) });
+}
+module.exports = {getDB,insertStudent,updateStudent,getStudentById,deleteStudent,insertUser}
+// exports.getDB = getDB;
+// exports.insertStudent = insertStudent;
+// exports.updateStudent = updateStudent;
